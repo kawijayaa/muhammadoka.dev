892fcf3b73e583b04c0ef836c6d8d453c0a97c22
Muhammad Oka <oka@muhammadoka.dev>
2024-06-24 04:56:17 +0700

    fix text size on home when on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Navbar home />
       <div className="flex flex-col">
         <img src="https://readme-typing-svg.herokuapp.com?font=JetBrains+Mono&weight=600&size=54&duration=3000&pause=1000&color=06b6d4&vCenter=true&random=false&width=600&height=70&lines=%2F%2F+muhammadoka.dev;%2F%2E%2A%5C%2E%28go%7Cpy%7Crs%29%24%2F" alt="Typing SVG" />
-        <p className="text-sm md:text-xl">telling computers what to do since 2012.</p>
+        <p className="text-md md:text-xl">telling computers what to do since 2012.</p>
       </div>
       <div className="hidden sm:flex flex-col text-right gap-4 md:gap-12 tracking-widest md:text-xl font-light">
         <a href="/about" className="hover:underline hover:text-cyan-500">about</a>
